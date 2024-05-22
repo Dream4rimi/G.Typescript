93e8a4e33759004a3bd6ad93121594cff005372a
Dream_4r <145902745+Unicorn4rich@users.noreply.github.com>
2024-05-22 22:03:45 +0500

    second comment

--- Class10/main.ts
+++ Class10/main.ts
@@ -1,17 +1,18 @@
 // fresh object and stail object a = b dono alag object
 // arrow function kya hai or isy array mein use karty hain
-// cover all loops total 4 loops
-// programiz website for loops details
-// length for loop se define karte hain 
-// lenght hmesha 1 se start hoti hai. or index zero se start hota hai
-//// revise employ project
+// for loop
+// while loop
+// do while loop 
+// for in loop
+// for of loop
+// visit programiz website for loops details
 
-// interface Employee {
-//     employeeId: any  
-// }
-
-// aik object banana hai 
-
+// Complete Employee Management System
+// Requirements:
+// 1) Use the same codebase where I left. from Sir Anas
+// 2) Use for loop to insert multiple employees
+// 3) Prepare edit, delete & updated function
+// 4) Use inquirer to get input for employee id, employee name & salary
 
 // <-----------------------------------------------END--------------------------------------------------->
 // fresh object and stale object ? a = b dono alag object
@@ -225,29 +226,17 @@
 
 
 // example 1
-// let shoaib: {name: string, age: number, adress: string} = {
 
+// let shoaib: { name: string, age: number, address: string } = {
 //     name: "shoaib",
 //     age: 23,
-//     adress: "mai nahi btao ga"
+//     address: "mai nahi btao ga"
 // };
 
-// // start for in loop
-// for (let data in shoaib){
-//     console.log(`${data} ${shoaib[data]}`)
+// // Start for in loop
+// for (let key in shoaib) {
+//     console.log(`${key}: ${shoaib[key]}`);  // working in erorr
 // }
-
-
-let shoaib: { name: string, age: number, address: string } = {
-    name: "shoaib",
-    age: 23,
-    address: "mai nahi btao ga"
-};
-
-// Start for in loop
-for (let key in shoaib) {
-    console.log(`${key}: ${shoaib[key]}`);
-}
 
 // <-----------------------------------------------END--------------------------------------------------->
 // for of loop
@@ -263,3 +252,99 @@ for (let key in shoaib) {
 
 
 // <-----------------------------------------------END--------------------------------------------------->
+// Complete Employee Management System
+// Requirements:
+// 1) Use the same codebase where I left.        //  from Sir Anas
+// 2) Use for loop to insert multiple employees
+// 3) Prepare edit, delete & updated function
+// 4) Use inquirer to get input for employee id, employee name & salary
+
+
+// this is work from Sir Muhamad Anas Seth
+
+// Defining Types
+interface Employee {
+    employeeId: number;
+    employeeName: string;
+    salary: number;
+}
+
+// Variable Initialization
+let collectionOfEmployee: Employee[] = [
+    {
+        employeeId: 7625,
+        employeeName: "Fahad Khan",
+        salary: 25000
+    },
+    {
+        employeeId: 7487,
+        employeeName: "Imran Sindhi",
+        salary: 50000
+    },
+    {
+        employeeId: 242536,
+        employeeName: "Atif Aslam",
+        salary: 100000,
+    },
+];
+
+let filterBySalary = collectionOfEmployee
+    .filter((employee) => employee.salary >= 50000);
+
+let filterByEmployeeId = collectionOfEmployee
+    .filter((employee: Employee) => employee.employeeId == 7487);
+
+let filterByEmployeeName = collectionOfEmployee
+    .filter((employee: Employee) => employee.employeeName.includes("Atif"))
+
+
+// <-----------------------------------------------END--------------------------------------------------->
+// Complete Employee Management System
+// Requirements:
+// 1) Use the same codebase where I left.        //  from Sir Anas
+// 2) Use for loop to insert multiple employees
+// 3) Prepare edit, delete & updated function
+// 4) Use inquirer to get input for employee id, employee name & salary
+
+import inquirer from 'inquirer'
+
+let main_data = [];
+let condition = false
+
+while(condition){
+let information = await inquirer.prompt([
+    {
+        name: "ask",
+        message: "What is your name",
+        type: "string"
+    },
+    {
+        name: "id",
+        message: "What is your id ?",
+        type: "number"
+    },
+    {
+        name: "salary",
+        message: "How much your Salery?",
+        type: "number"
+    }
+]);
+
+main_data.push(information);
+console.log(main_data)
+
+}
+// console.log(information);
+
+let again = await inquirer.prompt([
+    {
+        name: "ques",
+        message: "Do you want to add more employe?",
+        type: "confirm",
+        default: "true"
+    }
+]);
+
+if(again.ques === false){
+    condition = false
+};
