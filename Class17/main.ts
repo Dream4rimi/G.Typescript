7ee1db26ff26f7925278d1dee5f17711fd066681
Dream_4r <145902745+Unicorn4rich@users.noreply.github.com>
2024-08-03 23:52:19 +0500

    new comment

--- Class17/main.ts
+++ Class17/main.ts
@@ -5,6 +5,9 @@
 // Promises using .then .catch
 // Promises using  async await
 
+import { STATUS_CODES } from "http";
+import { CLIENT_RENEG_LIMIT } from "tls";
+
 
 // <------------------------------------------------END---------------------------------------------------->
 // In sab function ko define karny ka matlab ye hai ke hamen pata hona chahiye ke function mein 
@@ -106,46 +109,109 @@
 // or Concurrency ko achive karne ke liye ham Callbacks, Promises, or Async Await ka use kar ke
 // Multiple kaam aik hi time mein kar sakty hain...
 
-// case 1
-function washing(callback: () => void ){
-    console.log("Washing started...");
-
-    setTimeout(() => {
-        console.log("Washing done!");
-        callback();
-    }, 5000);
-
-};
-
-// case 2
-function soaking(callback: () => void ){
-    console.log("Soaking started...");
-
-    setTimeout(() => {
-        console.log('Soaking done!');
-        callback();
-    }, 3000);
-
-};
-
-// case 3
-function drying(){
-    console.log("Drying started...");
-
-    setTimeout(() => {
-        console.log("Drying done!");
-    }, 2000);
-
-};
+// Code Exicuting
+// 1. Washing started... 
+// 2. Washing done!
+// 3. Soaking started...
+// 4. Soaking done!
+// 5. Drying started...
+// 6. Drying done!
 
 
-// Now functions calling
-washing(() => {
+// // case 1
+// function washing(callback: () => void ){
+//     console.log("Washing started...");
 
-    soaking(() => {
+//     setTimeout(() => {
+//         console.log("Washing done!");
+//         callback();
+//     }, 5000);
 
-        drying();
+// };
 
-    })
+// // case 2
+// function soaking(callback: () => void ){
+//     console.log("Soaking started...");
 
-}yh )
+//     setTimeout(() => {
+//         console.log('Soaking done!');
+//         callback();
+//     }, 3000);
+
+// };
+
+// // case 3
+// function drying(){
+//     console.log("Drying started...");
+
+//     setTimeout(() => {
+//         console.log("Drying done!");
+//     }, 2000);
+
+// };
+
+// // console.log("folding loundry")
+
+// // Now functions calling
+// washing(() => {
+
+//     soaking(() => {
+
+//         drying();
+
+//     })
+
+// });
+
+// // console.log("I Am making biryani");
+
+
+// <------------------------------------------------END---------------------------------------------------->
+// Promises Structure using Async Await
+
+// Promise have 3 States => 
+// 1. Pending => promise abhi tak process mein hai.
+// 2. resolve => Promise Fullfiled ho chuka hai yani succesfully data mil gaya hai.
+// 3. reject  => Promise reject yani data nahi mila.
+
+// resolve => .then() method exicute
+// reject  => .catch() method exicute
+// finally => always exicute promise resolve or reject
+
+
+let promise = new Promise((resolve, reject) => {
+
+   setTimeout(() => {
+
+    // resolve("Success")
+    reject("Faliure!!!");
+
+   },3000);
+
+});
+
+promise
+       .then((value) => {
+        console.log(value);
+        console.log("Thank you for returning money")
+       })
+
+       .catch((value) => {
+        console.log(value)
+        console.log("Sorry I am unable to return money");
+       })
+
+
+
+// <------------------------------------------------END---------------------------------------------------->
+// Promises Code writing using Async Await
+
+// Code Exicuting
+// 1. Washing started... 
+// 2. Washing done!
+// 3. Soaking started...
+// 4. Soaking done!
+// 5. Drying started...
+// 6. Drying done!
+
+
